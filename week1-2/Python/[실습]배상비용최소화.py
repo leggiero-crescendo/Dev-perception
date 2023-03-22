8d79b27aab3cd68b796c909d5b127bc6d5646978
leggiero-crescendo <125112464+leggiero-crescendo@users.noreply.github.com>
2023-03-22 18:06:46 +0900

    Update [실습]배상비용최소화.py

--- week1-2/Python/[실습]배상비용최소화.py
+++ week1-2/Python/[실습]배상비용최소화.py
@@ -6,7 +6,9 @@
 🔐 Method
 
 🐣 TIL
-
+- lambda 사용한 코드를 보면 reverse를 하지 않고 -1 가장 끝을 확인하기 위해 -1과 breack를 사용하지 않는 방법을 씀 
+- 또한 lambda를 이용해서 work의 요소 x에 대하여 x**2를 한 후 for 문 대신 map을 사용하고 sum을 진행함
+=> map은 메모리 관리에 효율적임 [https://wikidocs.net/21057]
 '''
 
 def solution(no, works):
@@ -19,3 +21,10 @@ def solution(no, works):
     for i in works:
         result += i**2
     return result
+
+def solution_lambda(no, works):
+    for i in range(no):
+        works.sort()
+        if works[-1] != 0:
+            works[-1] -= 1
+    return sum(map(lambda x : x**2, works))
